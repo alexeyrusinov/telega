eded64736acce5ed12876f6e461c40f28d30e3ff
alexeyrusinov <ayrusinov@gmail.com>
2021-12-03 18:18:01 +0500

    remove token

--- start.py
+++ start.py
@@ -1,6 +1,7 @@
+from token import token
 from aiogram import Bot, Dispatcher, executor, types
 
-bot = Bot(token = "5049775754:AAFRWJM6XCcYsFQtMDQTkVcJlmJnmSR5YJY") # обьект бота
+bot = Bot(token) # обьект бота
 
 dp = Dispatcher(Bot)
 
