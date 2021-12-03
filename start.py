d08b1211984f094e7c4c34f75b81dbd2d86f80f1
alexeyrusinov <ayrusinov@gmail.com>
2021-12-03 18:25:55 +0500

    add handlers: velcome and echo

--- start.py
+++ start.py
@@ -1,9 +1,20 @@
-from token import token
+from key import token
 from aiogram import Bot, Dispatcher, executor, types
 
 bot = Bot(token) # обьект бота
 
-dp = Dispatcher(Bot)
+dp = Dispatcher(bot)
+
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    await message.reply("Hi! I'm EchoBot!")
+
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
 
 
 if __name__ == '__main__':
