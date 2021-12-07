0631c46cb06d0582e1565976827ae41c53bfe964
alexeyrusinov <ayrusinov@gmail.com>
2021-12-07 19:40:34 +0500

    clean file

--- start.py
+++ start.py
@@ -5,18 +5,13 @@ from aiogram import Bot, Dispatcher, executor, types
 from btcusdt import get_json_btcusdt
 
 
-# response = requests.get(url_binance)
-# response.raise_for_status()
-# dic = response.json()
-
-
 bot = Bot(token) # обьект бота
-
 dp = Dispatcher(bot)
 
 
 now = datetime.datetime.now()
 now_day_time = now.strftime("%H:%M\n%A %d/%m/%y")
+
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
@@ -27,18 +22,6 @@ async def send_welcome(message: types.Message):
 async def send_welcome(message: types.Message):
     await message.answer("Он поможет тебе: @rusinov")
 
-#
-# class FSMAdmin(StatesGroup):
-#     time_date = State
-#     end_work = State
-#
-# # начало диалога
-# @dp.message_handler(commands=['counting'], state=None)
-# async def cm_start(message: types.Message):
-#     await FSMAdmin.time_date.set()
-#
-
-
 
 @dp.message_handler()
 async def echo_message(message: types.Message):
@@ -46,13 +29,13 @@ async def echo_message(message: types.Message):
     if message.text == "Текущее время и дата":
         await bot.send_message(message.from_user.id, f"Current time: {now_day_time}")
     elif message.text == "Главное меню":
-        await bot.send_message(message.from_user.id, "Другое", reply_markup = nav.mainMenu)
+        await bot.send_message(message.from_user.id, "Главное меню", reply_markup = nav.mainMenu)
     elif message.text == "Другое":
         await bot.send_message(message.from_user.id, "Другое", reply_markup = nav.otherMenu)
     elif message.text == "Информация":
         await bot.send_message(message.from_user.id, "Какая-то информация...")
     elif message.text == "Курс биткоина":
-        await bot.send_message(message.from_user.id, f"Курс биткоина...\n{get_json_btcusdt(url_binance)}" )
+        await bot.send_message(message.from_user.id, get_json_btcusdt(url_binance))
 
 
 if __name__ == '__main__':
