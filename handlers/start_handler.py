65721c15a62d2ca63281f0f8c032d502c0aebfb9
alexeyrusinov <ayrusinov@gmail.com>
2022-01-24 20:44:10 +0500

    clean

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,26 +1,20 @@
 import os
-# import types
-# from aiogram import Dispatcher
-import markups as nav
 import sqlite_db
-# from start import dp
+import markups as nav
 from aiogram import types, Dispatcher
-
 
 ADMIN_ID = int(os.environ["ADMIN_ID"])
 
 
 # @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    user_id = message.from_user.id
     data_user = (message.from_user.id, message.from_user.username, message.from_user.first_name)
-    # if user_id == admin.ADMIN_ID:
-    if user_id == ADMIN_ID:
-        await sqlite_db.sql_add_command(user_id, data_user)
+    if data_user[0] == ADMIN_ID:
+        await sqlite_db.sql_add_user(data_user[0], data_user)
         await message.answer(f"Привет, выберите команду...", reply_markup=nav.adminMenu)
         await message.answer("Расписание проходящих автобусов", reply_markup=nav.inlineMenu)
     else:
-        await sqlite_db.sql_add_command(user_id, data_user)
+        await sqlite_db.sql_add_user(data_user[0], data_user)
         await message.answer(f"Привет, выберите команду...", reply_markup=nav.userMenu)
         await message.answer("Расписание проходящих автобусов", reply_markup=nav.inlineMenu)
 
