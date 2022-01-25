4993332cdfa896956dc679bafad93755d8a56d5c
alexeyrusinov <ayrusinov@gmail.com>
2022-01-25 22:02:18 +0500

    rename file to bus_fsm.py

--- fsm/bus_fsm.py
+++ fsm/bus_fsm.py
@@ -6,7 +6,7 @@ from func.pars_bus import get_current_schedule, get_all_bus_schedule,\
     get_buses_dispatched, get_buses_canceled
 import sqlite_db
 import markups as nav
-from handlers import start_handler
+from handlers.client_handlers import send_welcome
 
 
 class FSMAdmin(StatesGroup):
@@ -55,7 +55,7 @@ async def load_question(message: types.Message, state:FSMContext):
                         await message.reply(get_buses_canceled())
                     case 4:
                         await message.reply(get_current_schedule())
-                await start_handler.send_welcome(message)
+                await send_welcome(message)
             else:
                 await state.reset_state()
                 await message.answer("Только цифру из предложенных...")
@@ -66,7 +66,7 @@ async def load_question(message: types.Message, state:FSMContext):
             await fsm_start(message)
 
 
-def register_handlers_admin(dp : Dispatcher):
+def register_handlers_bus_fsm(dp : Dispatcher):
     dp.register_message_handler(fsm_start, commands="select", state=None)
     dp.register_message_handler(cancel_handler, state="*", commands="отмена")
     dp.register_message_handler(cancel_handler, Text(equals="отмена", ignore_case=True, ), state="*")
