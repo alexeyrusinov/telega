00850e2558653934afc2cbb2cbdff888d0f0e213
alexeyrusinov <ayrusinov@gmail.com>
2022-01-25 22:06:34 +0500

    moved other handlers

---
+++ handlers/other_handlers.py
@@ -0,0 +1,41 @@
+from aiogram import Dispatcher, types
+import markups as nav
+import sqlite_db
+from create_bot import bot, ADMIN_ID
+from func import btc
+from func.date_and_time import get_convert_date_time
+
+
+# @dp.message_handler()
+async def echo_message(message: types.Message):
+    match message.text:
+        case "Текущее время и дата":
+            await bot.send_message(message.from_user.id, get_convert_date_time())
+        case "Главное меню":
+            if message.from_user.id == ADMIN_ID:
+                await bot.send_message(message.from_user.id, "Главное меню", reply_markup=nav.adminMenu)
+            else:
+                await bot.send_message(message.from_user.id, "Главное меню", reply_markup=nav.userMenu)
+        case "Другое":
+            await bot.send_message(message.from_user.id, "Другое", reply_markup=nav.otherMenu)
+        case "Расписание автобуса":
+            # await bot.send_message(message.from_user.id, get_current_schedule())
+            await bot.send_message(message.from_user.id, sqlite_db.get_passing_bus(message.from_user.id))
+        case "Курс биткоина":
+            await bot.send_message(message.from_user.id, btc.get_btc_usdt_rate())
+        case "inlineButtons":
+            await bot.send_message(message.from_user.id, "inlineButtons", reply_markup=nav.myMenu)
+        case _:
+            if message.from_user.id != ADMIN_ID:
+                await bot.forward_message(ADMIN_ID, message.from_user.id, message.message_id)
+            if message.from_user.id == ADMIN_ID:
+                try:
+                    var = message.reply_to_message
+                    var = var["forward_from"]["id"]
+                    await bot.send_message(var, message.text)
+                except TypeError:
+                    pass
+
+
+def register_handlers_other(dp : Dispatcher):
+    dp.register_message_handler(echo_message)
