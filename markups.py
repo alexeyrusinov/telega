148d0b5c8455bfdfdf2c63a8c9c8c92598a41536
alexeyrusinov <ayrusinov@gmail.com>
2022-01-30 20:44:34 +0500

    del resize_keyboard=True from admin and user menu

--- markups.py
+++ markups.py
@@ -40,14 +40,14 @@ def user_and_admin_menu(user_id):
         btnOther = KeyboardButton("Другое")
         btn_all_db = KeyboardButton("alldb")
         btn_send = KeyboardButton("/send")
-        adminMenu = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(btnInfo).add(btnBtc, btnOther).add(btn_all_db, btn_send)
+        adminMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnInfo).add(btnBtc, btnOther).add(btn_all_db, btn_send)
         return adminMenu
     else:
         # user menu
         btnBtc = KeyboardButton("Курс биткоина")
         btnInfo = KeyboardButton("Расписание автобуса")
         btnOther = KeyboardButton("Другое")
-        userMenu = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(btnInfo).add(btnBtc, btnOther)
+        userMenu = ReplyKeyboardMarkup(resize_keyboard=True).add(btnInfo).add(btnBtc, btnOther)
         return userMenu
 
 ##############
@@ -64,10 +64,10 @@ btn_answer2 = KeyboardButton("отправленные")
 btn_answer3 = KeyboardButton("отмененные")
 btn_answer4 = KeyboardButton("ближайшие")
 btn_cancel = KeyboardButton("отмена")
-bus_answer_menu = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(btn_answer4)\
+bus_answer_menu = ReplyKeyboardMarkup(resize_keyboard=True).add(btn_answer4)\
                                                                                    .add(btn_answer1, btn_answer2)\
                                                                                    .add(btn_answer3, btn_cancel)
 
 # check menu
 btn_yes = KeyboardButton("отправляем")
-check_menu = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True).add(btn_yes, btn_cancel)
+check_menu = ReplyKeyboardMarkup(resize_keyboard=True).add(btn_yes, btn_cancel)
