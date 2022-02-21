21a84ffb56ce987ed2147c3060f2291e6bdab31f
alexeyrusinov <ayrusinov@gmail.com>
2022-02-21 20:22:09 +0500

    replace del message get_random_num to edit message and del inline_menu schedule

--- handlers/client_handlers.py
+++ handlers/client_handlers.py
@@ -5,8 +5,8 @@ from mark import markups as nav
 from create_bot import bot
 import random
 import os
-from func.pars_bus import get_all_bus_schedule, get_bus_dispatched, get_current_schedule, get_bus_canceled
-from mark.markups import generation_date_schedule
+from func.pars_bus import  get_current_schedule
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(os.path.basename(__file__))
@@ -16,7 +16,6 @@ logger = logging.getLogger(os.path.basename(__file__))
 async def send_welcome(message: types.Message):
     await sqlite_db.sql_add_user(message)
     await message.answer("select command..", reply_markup=nav.user_and_admin_menu(message.from_user.id))
-    await message.answer("test inline schedule", reply_markup=generation_date_schedule())
 
 
 # @dp.message_handler(commands=['help'])
@@ -30,32 +29,14 @@ async def test_generate_inline_menu(call: types.CallbackQuery):
         if i == day:
             await bot.delete_message(call.from_user.id, call.message.message_id)
             await bot.send_message(call.from_user.id, get_current_schedule(i), reply_markup=nav.generation_date_schedule())
-            logger.info(f"call data - {call.data} - {call.from_user.username} - {call.from_user.first_name}")
-
+            logger.info(f"call data: {call.data}, user: {call.from_user.username} - {call.from_user.first_name}")
 
 # @dp.callback_query_handler(text="btnRandom")
 async def get_random_num(message: types.Message):
-    await bot.delete_message(message.from_user.id, message.message.message_id)
-    await bot.send_message(message.from_user.id, "Случайное сисло: {0}".format(random.randint(0, 1000)), reply_markup=nav.myMenu)
-    # await bot.edit_message_text("Случайное число: {0}".format(random.randint(0, 1000)), message.from_user.id, message_id = message.message.message_id)
-
-
-# @dp.callback_query_handler(text_contains="bus")
-async def inline_menu(call: types.CallbackQuery):  # это чтобы понять какая кнопка была нажата
-    await bot.delete_message(call.from_user.id, call.message.message_id)
-    match call.data:
-        case "all_buses":
-            await bot.send_message(call.from_user.id, f"Все автобусы:\n {get_all_bus_schedule()}",
-                                   reply_markup=nav.inlineMenu)
-        case "dispatched_buses":
-            await bot.send_message(call.from_user.id, f"Отправленные:\n {get_bus_dispatched()}",
-                                   reply_markup=nav.inlineMenu)
-        case "bus_schedule":
-            await bot.send_message(call.from_user.id, f"Ближайшие:\n {get_current_schedule()}",
-                                   reply_markup=nav.inlineMenu)
-        case "buses_canceled":
-            await bot.send_message(call.from_user.id, f"Отменённые:\n {get_bus_canceled()}",
-                                   reply_markup=nav.inlineMenu)
+    # await bot.delete_message(message.from_user.id, message.message.message_id)
+    # await bot.send_message(message.from_user.id, "Случайное сисло: {0}".format(random.randint(0, 1000)), reply_markup=nav.myMenu)
+    await bot.edit_message_text("Случайное число: {0}".format(random.randint(0, 1000)), message.from_user.id,
+                                                    message_id = message.message.message_id, reply_markup=nav.myMenu)
 
 
 # @dp.callback_query_handler(text_contains="buy")
@@ -72,6 +53,5 @@ def register_handlers_client(dp : Dispatcher):
     dp.register_message_handler(send_help, commands=['help'])
     dp.register_callback_query_handler(test_generate_inline_menu, text_contains=["day"])
     dp.register_callback_query_handler(get_random_num, text="btnRandom")
-    dp.register_callback_query_handler(inline_menu, text_contains="bus")
     dp.register_callback_query_handler(bot_shop, text_contains="buy")
 
