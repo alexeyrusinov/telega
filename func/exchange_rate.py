3ac2271e2b92f33a5d843eb731d2b57ae53f5e83
alexeyrusinov <ayrusinov@gmail.com>
2022-03-21 22:22:08 +0500

    refactor func

--- func/exchange_rate.py
+++ func/exchange_rate.py
@@ -1,16 +1,16 @@
 import requests
 
 
-# btn Курс биткоина
-def get_btc_usdt_rate():
+# Курсы валют
+def get_exchange_rate(currency: int, slice_price: int, currency_sign: str):
     url_binance = "https://api.binance.com/api/v3/ticker/price"
     try:
         response = requests.get(url_binance)
         response.raise_for_status()
         dic = response.json()
-        res = dic[11]
-        result = str(res["symbol"] + " - " + res["price"])
-        result = result[:-6] + " $"
+        res = dic[currency]
+        result = str(res["symbol"] + " - *" +  res["price"][:slice_price] + f"* {currency_sign}")
         return result
     except Exception:
         print(">>>>--------> Errors with getting json <--------<<<<")
+        return 'ошибка получения курса, напиши -> @rusinov'
