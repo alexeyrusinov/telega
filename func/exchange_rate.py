eb673837604c7bc3c09f7f8518f93527b2ce4ec1
alexeyrusinov <ayrusinov@gmail.com>
2022-03-24 19:00:24 +0500

    moved text bold

--- func/exchange_rate.py
+++ func/exchange_rate.py
@@ -11,7 +11,7 @@ def get_exchange_rate(currency: int, currency_sign: str):
         result_currency = dic[currency]
         my_slice = result_currency["price"].split(".")
         currency_pair = result_currency["symbol"]
-        total = f"{currency_pair} - *{my_slice[0]}.{my_slice[1][:2]} {currency_sign}*"
+        total = f"{currency_pair} - *{my_slice[0]}.{my_slice[1][:2]}* {currency_sign}"
         return total
     except Exception:
         print(">>>>--------> Errors with getting json <--------<<<<")
