a294cca7ef70585ae12bcc1149e66a925ad63e27
alexeyrusinov <ayrusinov@gmail.com>
2022-04-04 18:55:38 +0500

    add exception 'Exchange rate' for admin

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -24,6 +24,10 @@ async def filter_message(message: types.Message):
     # elif message.text == "Расписание проходящего":
     #     await bot.send_message(message.from_user.id, my_db.get_timetable_passing_bus(message.from_user.id, id_station_arr=1331, days=0))
 
+    elif message.text == "Exchange rate":
+        await bot.send_message(message.from_user.id, exchange_rate.get_exchange_rate_for_admin(11, '$', 688, '₽'), parse_mode="Markdown")
+
+
     elif message.text == "BTC/USDT":
         await bot.send_message(message.from_user.id, exchange_rate.get_exchange_rate(11, '$'), parse_mode="Markdown")
     elif message.text == "USDT/RUB":
