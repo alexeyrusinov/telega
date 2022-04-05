a1bc3036f0c5df63e75f76a9cd7bd26450359a48
alexeyrusinov <ayrusinov@gmail.com>
2022-04-05 20:38:00 +0500

    fix get_exchange_rate_for_admin = add args kwargs

--- func/exchange_rate.py
+++ func/exchange_rate.py
@@ -18,23 +18,25 @@ def get_exchange_rate(currency: int, currency_sign: str):
         return 'ошибка получения курса, напиши -> @rusinov'
 
 
-def get_exchange_rate_for_admin(currency1: int, currency_sign1: str, currency2: int, currency_sign2: str):
+def get_exchange_rate_for_admin(*args, **kwargs):
     url_binance = "https://api.binance.com/api/v3/ticker/price"
+    total = ''
+    sign = []
+    for k, v in kwargs.items():
+        sign.append(v)
+    counter = 0
     try:
         response = requests.get(url_binance)
         response.raise_for_status()
         dic = response.json()
-        result_currency1 = dic[currency1]
-        result_currency2 = dic[currency2]
-        my_slice1 = result_currency1["price"].split(".")
-        my_slice2 = result_currency2["price"].split(".")
-        currency_pair1 = result_currency1["symbol"]
-        currency_pair2 = result_currency2["symbol"]
-        total = f"{currency_pair1} - *{my_slice1[0]}.{my_slice1[1][:2]}* {currency_sign1}"
-        total += f"\n{currency_pair2} - *{my_slice2[0]}.{my_slice2[1][:2]}* {currency_sign2}"
+        for i in args:
+            result_currency = dic[i]
+            my_slice1 = result_currency["price"].split(".")
+            currency_pair1 = result_currency["symbol"]
+            total += f"{currency_pair1} - *{my_slice1[0]}.{my_slice1[1][:2]}* {sign[counter]}\n"
+            counter += 1
         return total
     except Exception:
         print(">>>>--------> Errors with getting json <--------<<<<")
         return 'ошибка получения курса, напиши -> @rusinov'
-
 
