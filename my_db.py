7cc7ed867ef2fa220bfa38e1041383d0512700b4
alexeyrusinov <ayrusinov@gmail.com>
2022-04-10 17:10:35 +0500

    rename road to db at all func, add func load_file, create_table, insert_station

--- my_db.py
+++ my_db.py
@@ -1,15 +1,16 @@
-from func import pars_bus
-import sqlite3 as sq
 from aiogram import types
+import json
 import logging
 import os
+from func import pars_bus
+import sqlite3 as sq
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(os.path.basename(__file__))
 
 
 def sql_start():
-    with sq.connect("../users.db") as con:
+    with sq.connect("files/users.db") as con:
         cur = con.cursor()
         logger.info("Data base connected Ok!")
         cur.execute('CREATE TABLE IF NOT EXISTS users(user_id INT PRIMARY KEY,'
@@ -23,7 +24,7 @@ def sql_start():
 
 
 async def sql_add_user(message: types.Message):
-    with sq.connect("../users.db") as con:
+    with sq.connect("files/users.db") as con:
         data_user = (message.from_user.id, message.from_user.username, message.from_user.first_name)
         cur = con.cursor()
         cur.execute("SELECT user_id FROM users WHERE user_id = ?", (data_user[0],))
@@ -38,7 +39,7 @@ async def sql_add_user(message: types.Message):
 
 
 def get_timetable_passing_bus(user_id, id_station_arr, days):
-    with sq.connect("../users.db") as con:
+    with sq.connect("files/users.db") as con:
         cur = con.cursor()
         cur.execute("""SELECT passing_bus FROM users WHERE user_id = ?""", (user_id,))
         result = cur.fetchone()
@@ -63,7 +64,7 @@ def get_timetable_passing_bus(user_id, id_station_arr, days):
 
 
 async def update_type_timetable_passing_bus(data_user, result):
-    with sq.connect("../users.db") as con:
+    with sq.connect("files/users.db") as con:
         cur = con.cursor()
         cur.execute("""UPDATE users SET passing_bus = ? WHERE user_id = ?""", (result, data_user[0],))
         con.commit()
@@ -72,37 +73,55 @@ async def update_type_timetable_passing_bus(data_user, result):
 
 
 async def update_sp_and_fp_call(data_user, start_place_call, finish_place_call):
-    with sq.connect("../users.db") as con:
+    with sq.connect("files/users.db") as con:
         cur = con.cursor()
         cur.execute("""SELECT start_place_call, finish_place_call  FROM users WHERE user_id = ?""", (data_user[0],))
         items = cur.fetchall()
         if items[0][0] == start_place_call and items[0][1] == str(finish_place_call):
             logger.info(f"start_place_call - {start_place_call}, finish_place_call - {finish_place_call}")
         else:
-            with sq.connect("../users.db") as con:
+            with sq.connect("files/users.db") as con:
                 cur = con.cursor()
                 cur.execute("""UPDATE users SET start_place_call = ?, finish_place_call = ?  WHERE user_id = ?""", (start_place_call, finish_place_call, data_user[0],))
                 con.commit()
                 logger.info(f"value of start_place_call = {start_place_call}  finish_place_call = {finish_place_call} of user {data_user}")
         cur.close()
 
-
-# def get_sp_and_fp_user_call(message: types.Message, user_id, days):
-#     with sq.connect("../users.db") as con:
-#         cur = con.cursor()
-#         cur.execute("""SELECT start_place_call, finish_place_call  FROM users WHERE user_id = ?""", (user_id,))
-#         result = cur.fetchall()
-#         cur.close()
-#         print(type(result))
-#         print(result)
-#         if result[0][0] and result[0][1]:
-#             return pars_bus.get_current_schedule(result[0][0], result[0][1], days)
-#         # if result:
-#         #     return pars_bus.get_current_schedule(result[0][0], result[0][1], days)
-#         else:
-#             station_fsm_set(message) # --------
-
-        # elif result:
-        #     return pars_bus.get_current_schedule(result[0][0], result[0][1], days)
+# __________________________________
 
 
+def load_file(file):
+    with open(file) as f:  # Read json file
+        stations = json.load(f)
+        stations = dict(reversed(list(stations.items())))  # revers
+    return stations
+
+
+def create_table():
+    with sq.connect("files/users.db") as con:
+        cur = con.cursor()
+        logger.info("table created :start_place")
+        print("Data base connected Ok!")
+        cur.execute('CREATE TABLE IF NOT EXISTS start_place(name_station TEXT PRIMARY KEY,'
+                    'station_call TEXT)')
+        con.commit()
+        cur.close()
+
+
+create_table()
+
+
+def insert_station():
+    with sq.connect("files/users.db") as con:
+        cur = con.cursor()
+        stations = load_file('files/starting_point_24.json')
+        stations_k = []
+        for k, v in reversed(stations.items()):
+            stations_k.append((k, v))
+        cur.executemany(
+            "INSERT INTO start_place VALUES (?, ?)", stations_k)
+        con.commit()
+        cur.close()
+
+
+insert_station()
