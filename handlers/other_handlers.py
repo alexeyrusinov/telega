17d4e4610cb244f98c3e5dc8a865b5409b7d618b
alexeyrusinov <ayrusinov@gmail.com>
2022-05-03 13:41:57 +0500

    add sql_add_user to filter_message if message.text == "Текущее время и дата", "Главное меню", "Другое", "Exchange rate", "BTC/USDT", "USDT/RUB", "Расписание на несколько дней", в исключении "Расписание на несколько дней" добавлено обращение к бд и на основе userdata отправлять данные

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -1,73 +1,66 @@
 from aiogram import Dispatcher, types
 import sqlite3 as sq
-from fsm.sp_fp_fsm import station_fsm_set
-from mark import markups as nav
 import my_db
+from fsm.sp_fp_fsm import station_fsm_set, get_name_station
+from mark import markups as nav
 from create_bot import bot, ADMIN_ID
-from func import exchange_rate, pars_bus
+from func import exchange_rate
 from func.date_and_time import get_convert_date_time
 from mark.markups import generation_date_schedule
 
 
 async def filter_message(message: types.Message):
     if message.text == "Текущее время и дата":
+        await my_db.sql_add_user(message)
         await bot.send_message(message.from_user.id, get_convert_date_time())
     elif message.text == "Главное меню":
+        await my_db.sql_add_user(message)
         await bot.send_message(message.from_user.id, "Главное меню",
                                reply_markup=nav.user_and_admin_menu(message.from_user.id))
     elif message.text == "Другое":
+        await my_db.sql_add_user(message)
         await bot.send_message(message.from_user.id, "Другое", reply_markup=nav.otherMenu)
-
-    # elif message.text == "Расписание проходящего":
-    #     await bot.send_message(message.from_user.id, my_db.get_timetable_passing_bus(message.from_user.id, id_station_arr=1331, days=0))
-
     elif message.text == "Exchange rate":
-        await bot.send_message(message.from_user.id, exchange_rate.get_exchange_rate_from_api(11, 688, sign_dollar='$', sign_rubl='₽'), parse_mode="Markdown")
+        await my_db.sql_add_user(message)
+        await bot.send_message(message.from_user.id,
+                               exchange_rate.get_exchange_rate_from_api(11, 688, sign_dollar='$', sign_rubl='₽'),
+                               parse_mode="Markdown")
     elif message.text == "BTC/USDT":
+        await my_db.sql_add_user(message)
         await bot.send_message(message.from_user.id, exchange_rate.get_exchange_rate(11, '$'), parse_mode="Markdown")
     elif message.text == "USDT/RUB":
+        await my_db.sql_add_user(message)
         await bot.send_message(message.from_user.id, exchange_rate.get_exchange_rate(688, '₽'), parse_mode="Markdown")
     elif message.text == "inlineButtons":
+        await my_db.sql_add_user(message)
         await bot.send_message(message.from_user.id, "inlineButtons", reply_markup=nav.myMenu)
-    elif message.text == "Расписание проходящего на несколько дней":
-        await message.answer("test inline schedule", reply_markup=generation_date_schedule(id_station_arr=1331))
+    elif message.text == "Расписание на несколько дней":
+        await my_db.sql_add_user(message)
+        data_user = message.from_user.id
+        with sq.connect("files/users.db") as con:
+            cur = con.cursor()
+            cur.execute("""SELECT start_place_call, finish_place_call  FROM users WHERE user_id = ?""", (data_user,))
+            items = cur.fetchall()
+            if items[0][0] == str(None) and items[0][1] == str(None):
+                await station_fsm_set(message, flag_next_schedule=True)
+            else:
+                start_place = get_name_station('files/starting_point_24.json', items[0][0])
+                finish_place = get_name_station('files/704.json', items[0][1])
+                await message.answer(f"от *{start_place}* до *{finish_place}*",
+                                     reply_markup=generation_date_schedule(start_place=items[0][0], finish_place=items[0][1]),
+                                     parse_mode="Markdown")
     else:
         if message.from_user.id != ADMIN_ID:
+            await my_db.sql_add_user(message)
             await bot.forward_message(ADMIN_ID, message.from_user.id, message.message_id)
         if message.from_user.id == ADMIN_ID:
+            await my_db.sql_add_user(message)
             try:
                 var = message.reply_to_message
                 var = var["forward_from"]["id"]
                 await bot.send_message(var, message.text)
             except TypeError:
                 pass
-
-
-# match message.text:
-#     case "Текущее время и дата":
-#         await bot.send_message(message.from_user.id, get_convert_date_time())
-#     case "Главное меню":
-#         await bot.send_message(message.from_user.id, "Главное меню", reply_markup=nav.user_and_admin_menu(message.from_user.id))
-#     case "Другое":
-#         await bot.send_message(message.from_user.id, "Другое", reply_markup=nav.otherMenu)
-#     case "Расписание автобуса":
-#         await bot.send_message(message.from_user.id, sqlite_db.get_timetable_passing_bus(message.from_user.id))
-#     case "Курс биткоина":
-#         await bot.send_message(message.from_user.id, btc.get_btc_usdt_rate())
-#     case "inlineButtons":
-#         await bot.send_message(message.from_user.id, "inlineButtons", reply_markup=nav.myMenu)
-#     case "inline schedule":
-#         await message.answer("test inline schedule", reply_markup=generation_date_schedule())
-#     case _:
-#         if message.from_user.id != ADMIN_ID:
-#             await bot.forward_message(ADMIN_ID, message.from_user.id, message.message_id)
-#         if message.from_user.id == ADMIN_ID:
-#             try:
-#                 var = message.reply_to_message
-#                 var = var["forward_from"]["id"]
-#                 await bot.send_message(var, message.text)
-#             except TypeError:
-#                 pass
 
 
 def register_handlers_other(dp: Dispatcher):
